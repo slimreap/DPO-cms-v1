a83bb668fececa0bb7819b72b9eedaf88e4e9e4d
Sarhan Asakil <87589442+slimreap@users.noreply.github.com>
2024-05-08 12:45:28 +0800

    Delete vendor directory

--- vendor/filament/filament/resources/views/login.blade.php
+++
@@ -1,7 +0,0 @@
-<form wire:submit.prevent="authenticate" class="space-y-8">
-    {{ $this->form }}
-    <div class="w-full flex justify-center underline"><a class="mx-auto" href="{{route('sendotp')}}">reset password</a></div>
-    <x-filament::button type="submit" form="authenticate" class="w-full">
-        {{ __('filament::login.buttons.submit.label') }}
-    </x-filament::button>
-</form>
